dff1337450b9524952f7ad378623fbc4d4281b9f
Aaron Hewson <aaronjhewson@gmail.com>
2025-09-10 18:57:41 +1200

    sectioned code

--- PLINK Subset Testing.R
+++ PLINK Subset Testing.R
@@ -1,10 +1,16 @@
-#This script is for comparing the performance of PLINK in duplicate matching, between using all SNPs selected or a subset
+# Comparing the performance of PLINK for duplicate analysis, based on all high-quality SNPs or a selected subset.
+# High-quality SNPs are those chosen by Axiom Analysis Suite as polymorphic and high-resolution.
+# A subset of SNPs for comparison were chosen. These had missing call rate less than 0.01, minor allele frequency more than 0.3, and were pruned for linkage equilibrium.
+# These were compared, as PLINK duplicate analysis relies on SNPs being in approximate linkage equilibrium (https://doi.org/10.1086/519795).
 
-#set working directory [must contain plink.exe and files for analysis]
+
+# Set Working Directory ---------------------------------------------------
+
+#set working directory
 setwd("C:/Users/curly/Desktop/Apple Genotyping/Methods/PLINK Subset Testing/Inputs")
 
 
-##Run PLINK with all SNPs
+# Run PLINK with All SNPs -------------------------------------------------
 
 #PLINK dup matching
 system("plink --file JD_PFR_PLINK --missing-genotype 0 --genome full")
@@ -14,7 +20,7 @@ genome <- read.table("plink.genome", header = TRUE, sep = "", stringsAsFactors =
 write.table(genome, "plink_all_snps.txt", sep = "\t", row.names = FALSE, quote = FALSE)
 
 
-##Run PLINK with SNP subset
+# Create SNP Subset and Run PLINK -----------------------------------------
 
 #Create subset of SNPs in PLINK
 system("plink --file JD_PFR_PLINK --missing-genotype 0 --geno 0.01 --maf 0.3 --indep 50 5 1.5")
